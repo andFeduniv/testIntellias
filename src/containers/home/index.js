d4c22c718e624a99b8c01ca3766fb1b7fe2ff3cc
Andriy Feduniv <and.feduniv@gmail.com>
2018-02-13 12:01:04 +0200

    Fix scss

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import GitUser from '../git-user'
-import './home.scss';
+import './home.css';
 
 class Home extends React.Component {
   render() {
     return <div className="home">
-     <h1>Test Task Intellias</h1>
+     <h1>Test Task Intellias111</h1>
       <GitUser />
     </div>
   }
